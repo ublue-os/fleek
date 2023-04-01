a194fb3bc13a8dc25c0f8311b8696d199767fc7a
Brian Ketelsen <bketelsen@gmail.com>
2023-03-31 22:06:47 -0400

    fix: name

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -3,5 +3,5 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run cmd/fleek/main.go completion "$sh" >"completions/goreleaser.$sh"
+	go run cmd/fleek/main.go completion "$sh" >"completions/fleek.$sh"
 done
