b06187d179484848299452e47002b7f194220637
Brian Ketelsen <bketelsen@gmail.com>
2023-04-21 12:18:18 -0400

    fix: debugging templates

--- internal/flake/flake.go
+++ internal/flake/flake.go
@@ -46,7 +46,7 @@ func Load(cfg *fleek.Config, app *app.App) (*Flake, error) {
 		fin.Debug.Println("Walking", path)
 		fin.Debug.Println("IsDir", d.IsDir())
 		fin.Debug.Println("Is a Template", strings.Contains(path, ".tmpl"))
-		if !d.IsDir() && strings.Contains(path, ".tmpl") {
+		if filepath.Ext(path) == ".tmpl" {
 			bb, err := templates.ReadFile(path)
 			if err != nil {
 				return err
