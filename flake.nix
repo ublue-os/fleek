d0bbdde061d609e59c2fcdbbc633c60870236541
Brian Ketelsen <bketelsen@gmail.com>
2023-06-15 15:59:25 +0000

    chore: bump version v0.9.8

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   outputs = { self, nixpkgs }: let
     # Current version
-    version = "v0.9.7";
+    version = "v0.9.8";
     # Supported systems
     systems = [
       "aarch64-linux" # 64-bit ARM Linux
