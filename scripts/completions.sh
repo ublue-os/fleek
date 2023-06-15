ded7d09d3916ff06f93a569affe0067673f67867
Brian Ketelsen <bketelsen@gmail.com>
2023-06-15 01:49:00 +0000

    fix: is it really a fix

--- scripts/completions.sh
+++ scripts/completions.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e
-rm -rf completions
 export WARN_FLEEK=no
-mkdir completions
 for sh in bash zsh fish; do
 	echo "$sh"
-	go run cmd/fleek/main.go completion "$sh" >"completions/fleek.$sh"
+	go run ./cmd/fleek/main.go completion "$sh" >"completions/fleek.$sh"
 done
