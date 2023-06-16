6f7b544137e5a586347acf2572a06e11115a11fa
Brian Ketelsen <bketelsen@gmail.com>
2023-06-16 10:23:24 -0400

    fix: respect location flag (#218)
    * fix: respect location flag
    
    Move location flag out of init command to be a persistent flag of the root command
    
    * fix: remove unused variable

--- internal/fleekcli/init.go
+++ internal/fleekcli/init.go
@@ -8,14 +8,12 @@ import (
 	"github.com/ublue-os/fleek/fin"
 	"github.com/ublue-os/fleek/internal/flake"
 	"github.com/ublue-os/fleek/internal/fleekcli/usererr"
-	"github.com/ublue-os/fleek/internal/xdg"
 )
 
 type initCmdFlags struct {
-	apply    bool
-	force    bool
-	location string
-	level    string
+	apply bool
+	force bool
+	level string
 }
 
 func InitCommand() *cobra.Command {
@@ -34,8 +32,6 @@ func InitCommand() *cobra.Command {
 		&flags.apply, app.Trans("init.applyFlag"), "a", false, app.Trans("init.applyFlagDescription"))
 	command.Flags().BoolVarP(
 		&flags.force, app.Trans("init.forceFlag"), "f", false, app.Trans("init.forceFlagDescription"))
-	command.Flags().StringVarP(
-		&flags.location, app.Trans("init.locationFlag"), "l", xdg.DataSubpathRel("fleek"), app.Trans("init.locationFlagDescription"))
 	command.Flags().StringVar(
 		&flags.level, app.Trans("init.levelFlag"), "default", app.Trans("init.levelFlagDescription"))
 	return command
